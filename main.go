b325b5f570cd40ecc3e6bced31626c431cef9ade
zhangruoxiao <zhangruoxiao@doumi.com>
2021-07-28 15:30:53 +0800

    读取表单数据

--- main.go
+++ main.go
@@ -55,7 +55,16 @@ func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "创建新的文章")
+	if err := r.ParseForm(); err != nil {
+		// 解析错误, 这里有错误处理
+		fmt.Fprint(w,  "请提供正确的数据！")
+		return
+	}
+
+	fmt.Fprintf(w, "r.Form 中 title 的值为: %v <br>", r.FormValue("title"))
+	fmt.Fprintf(w, "r.PostForm 中 title 的值为: %v <br>", r.PostFormValue("title"))
+	fmt.Fprintf(w, "r.Form 中 test 的值为: %v <br>", r.FormValue("test"))
+	fmt.Fprintf(w, "r.PostForm 中 test 的值为: %v <br>", r.PostFormValue("test"))
 }
 
 func forceHTMLMiddleware(next http.Handler) http.Handler {
