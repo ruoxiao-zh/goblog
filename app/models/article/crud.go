78560b18099e48546969b250bbbf17d24f86bd4e
zhangruoxiao <zhangruoxiao@doumi.com>
2021-07-29 15:29:38 +0800

    重构删除文章

--- app/models/article/crud.go
+++ app/models/article/crud.go
@@ -50,3 +50,15 @@ func (a *Article) Update() (rowsAffected int64, err error) {
 
 	return result.RowsAffected, nil
 }
+
+// Delete 删除文章
+func (a *Article) Delete() (rowsAffected int64, err error) {
+	result := model.DB.Delete(&a)
+	if err = result.Error; err != nil {
+		logger.LogError(err)
+
+		return 0, err
+	}
+
+	return result.RowsAffected, nil
+}
