7bad82b317a0ac7a1a66804d91512f3ef171c6b1
zhangruoxiao <zhangruoxiao@doumi.com>
2021-08-01 10:11:18 +0800

    授权中间件

---
+++ app/http/middlewares/auth.go
@@ -0,0 +1,23 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"goblog/pkg/auth"
+	"goblog/pkg/flash"
+)
+
+// Auth 登录用户才可访问
+func Auth(next HttpHandlerFunc) HttpHandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		if !auth.Check() {
+			flash.Warning("登录用户才能访问此页面")
+			http.Redirect(w, r, "/", http.StatusFound)
+
+			return
+		}
+
+		next(w, r)
+	}
+}
