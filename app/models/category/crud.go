f8c12ff5f7caf70649fbd79d514f8de7d5c149d4
zhangruoxiao <zhangruoxiao@doumi.com>
2021-08-02 15:39:05 +0800

    关联文章和分类

--- app/models/category/crud.go
+++ app/models/category/crud.go
@@ -3,11 +3,12 @@ package category
 import (
 	"goblog/pkg/logger"
 	"goblog/pkg/model"
+	"goblog/pkg/types"
 )
 
 // Create 创建分类，通过 category.ID 来判断是否创建成功
-func (category *Category) Create() (err error) {
-	if err = model.DB.Create(&category).Error; err != nil {
+func (c *Category) Create() (err error) {
+	if err = model.DB.Create(&c).Error; err != nil {
 		logger.LogError(err)
 		return err
 	}
@@ -23,4 +24,15 @@ func All() ([]Category, error) {
 	}
 
 	return categories, nil
+}
+
+// Get 通过 ID 获取分类
+func Get(idstr string) (Category, error) {
+	var category Category
+	id := types.StringToInt(idstr)
+	if err := model.DB.First(&category, id).Error; err != nil {
+		return category, err
+	}
+
+	return category, nil
 }
